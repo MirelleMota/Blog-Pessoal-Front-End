2b3beb44454a19d0804b1530efbd3e36aff16b29
Mirelle Mota <mirelle.motta06@gmail.com>
2022-09-28 12:25:38 -0300

    Primeiro componente React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+       <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Mario_Series_Logo.svg/640px-Mario_Series_Logo.svg.png" alt="Imagem Tela Inicial" className="img"/>
+       </>
+    );
+
+}
+
+export default Home;
