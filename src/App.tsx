b2e12fa43956dfc758a8cfd411956f791a3ba4cc
Mirelle Mota <mirelle.motta06@gmail.com>
2022-10-01 19:39:57 -0300

    Componentes React

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import {Grid} from '@material-ui/core';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
+
+
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+    <BrowserRouter>
+      
+        <Navbar />
+
+        <div style={{minHeight: '100vh'}}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+
+          </Routes>
+        </div>
+        
+        <Footer />
+      
+    </BrowserRouter>
   );
 }
 
